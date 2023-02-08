1d4e6b167cb098923d2dd142a8c41ec162ab3eb4
Neil Dwyer <neildwyer1991@gmail.com>
2023-02-08 11:31:40 -0800

    fix typescript error

--- src/controller/WorldBoundaryController.tsx
+++ src/controller/WorldBoundaryController.tsx
@@ -8,37 +8,49 @@ const maxYBoundary = 1000;
 
 type Props = {
   myPlayer: Player;
-  setMyPlayer: Dispatch<SetStateAction<Player>>;
+  setMyPlayer: Dispatch<SetStateAction<Player | null>>;
 };
 
 export const WorldBoundaryController = ({ myPlayer, setMyPlayer }: Props) => {
   useEffect(() => {
     if (myPlayer.position.x < minXBoundary) {
-      setMyPlayer((prev) => ({
-        ...prev,
-        position: { x: minXBoundary, y: prev.position.y },
-      }));
+      setMyPlayer(
+        (prev) =>
+          prev && {
+            ...prev,
+            position: { x: minXBoundary, y: prev.position.y },
+          }
+      );
       myPlayer.position.x = minXBoundary;
     }
     if (myPlayer.position.x > maxXBoundary) {
-      setMyPlayer((prev) => ({
-        ...prev,
-        position: { x: maxXBoundary, y: prev.position.y },
-      }));
+      setMyPlayer(
+        (prev) =>
+          prev && {
+            ...prev,
+            position: { x: maxXBoundary, y: prev.position.y },
+          }
+      );
       myPlayer.position.x = maxXBoundary;
     }
     if (myPlayer.position.y < minYBoundary) {
-      setMyPlayer((prev) => ({
-        ...prev,
-        position: { x: prev.position.x, y: minYBoundary },
-      }));
+      setMyPlayer(
+        (prev) =>
+          prev && {
+            ...prev,
+            position: { x: prev.position.x, y: minYBoundary },
+          }
+      );
       myPlayer.position.y = minYBoundary;
     }
     if (myPlayer.position.y > maxYBoundary) {
-      setMyPlayer((prev) => ({
-        ...prev,
-        position: { x: prev.position.x, y: maxYBoundary },
-      }));
+      setMyPlayer(
+        (prev) =>
+          prev && {
+            ...prev,
+            position: { x: prev.position.x, y: maxYBoundary },
+          }
+      );
       myPlayer.position.y = maxYBoundary;
     }
   }, [myPlayer, setMyPlayer]);
