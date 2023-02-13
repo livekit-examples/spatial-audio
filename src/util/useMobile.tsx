85ad96b037f540404512ad9a4611ee09cbcea6e3
Neil Dwyer <neildwyer1991@gmail.com>
2023-02-13 15:17:45 -0800

    handle when no navigator

--- src/util/useMobile.tsx
+++ src/util/useMobile.tsx
@@ -1,6 +1,6 @@
 import { useDeviceSelectors } from "react-device-detect";
 
 export const useMobile = () => {
-  const [selectors] = useDeviceSelectors(navigator.userAgent || "SSR");
+  const [selectors] = useDeviceSelectors(navigator?.userAgent || "SSR");
   return selectors.isMobile;
 };
