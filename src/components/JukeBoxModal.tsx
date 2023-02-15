0ba97690490655e7b9778f274af0dc6eb712367c
Neil Dwyer <neildwyer1991@gmail.com>
2023-02-15 15:06:41 -0800

    working on jukebox modal

--- src/components/JukeBoxModal.tsx
+++ src/components/JukeBoxModal.tsx
@@ -1,4 +1,16 @@
+import {
+  useLocalParticipant,
+  useRemoteParticipant,
+  useRemoteParticipants,
+  useTracks,
+} from "@livekit/components-react";
+import { LocalParticipant } from "livekit-client";
+import { useEffect } from "react";
+
 export const JukeBoxModal = () => {
+  const remoteParticipants = useRemoteParticipants();
+  const { localParticipant } = useLocalParticipant();
+
   return (
     <div className="flex flex-col items-center p-4 bg-white rounded-md">
       <div>Disco JukeBox</div>
