b04d9270bf86684aa044e4f0b49db2b25fd6d368
Neil Dwyer <neildwyer1991@gmail.com>
2023-04-24 12:04:39 -0700

    Add depoy with vercel button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useCallback, useState } from "react";
 import { toast, Toaster } from "react-hot-toast";
@@ -19,7 +20,7 @@ export default function Home() {
   return (
     <main>
       <Toaster />
-      <div className="flex flex-col items-center justify-center h-screen w-screen">
+      <div className="relative flex flex-col items-center justify-center h-screen w-screen">
         <h1 className="text-4xl mb-8 p-2">Spatial Audio LiveKit Example App</h1>
         <form
           onSubmit={(e) => {
@@ -40,6 +41,12 @@ export default function Home() {
             </div>
           </div>
         </form>
+        <div className="absolute bottom-2 right-2">
+          <a href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Flivekit-examples%2Fspatial-audio&env=LIVEKIT_API_KEY,LIVEKIT_API_SECRET,LIVEKIT_WS_URL&envDescription=Get%20these%20from%20your%20cloud%20livekit%20project.&envLink=https%3A%2F%2Fcloud.livekit.io&project-name=my-spatial-audio-app">
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img alt="deploy with Vercel" src="https://vercel.com/button" />
+          </a>
+        </div>
       </div>
     </main>
   );
